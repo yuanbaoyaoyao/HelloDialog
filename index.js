0165a999ff981369853c9b423467dd6184a00ec2
yuanbaoyaoyao <583515921@qq.com>
2022-08-30 09:11:44 +0800

    create base structure

---
+++ index.js
@@ -0,0 +1,16 @@
+const helloDialog = `
+<div class="hello-dialog-backdrop">
+<div class="hello-dialog">
+  <div class="hello-dialog-header">
+    <p>这是标题</p>
+    <button class="hello-dialog-close"></button>
+  </div>
+  <div class="hello-dialog-body">这是body</div>
+  <div class="hello-dialog-bottom">
+    <button class="hello-dialog-confirm">确认</button>
+  </div>
+</div>
+</div>
+`
+
+export default helloDialog
