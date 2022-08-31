aca5a5658f4a67d829a83a3a46a6068979a283e5
yuanbaoyaoyao <583515921@qq.com>
2022-08-31 12:39:24 +0800

    add function move

--- index.js
+++ index.js
@@ -38,6 +38,25 @@ class helloDialog {
         this.helloDialogDom = helloDialogDom
         this.helloDialogHeaderDom = helloDialogHeaderDom
         this.isFullScreen = isFullScreen
+
+        let distanceX = 0;
+        let distanceY = 0;
+        let firstDistanceX = 0;
+        let firstDistanceY = 0;
+        let tempRight = 0;
+        let tempLeft = 0;
+        let tempTop = 0;
+        let tempBottom = 0;
+
+        this.distanceX = distanceX;
+        this.distanceY = distanceY;
+        this.firstDistanceX = firstDistanceX;
+        this.firstDistanceY = firstDistanceY;
+        this.tempRight = tempRight;
+        this.tempLeft = tempLeft;
+        this.tempTop = tempTop;
+        this.tempBottom = tempBottom;
+
     }
     show = (backdrop) => {
         document.body.appendChild(backdrop)
@@ -62,6 +81,7 @@ class helloDialog {
         backdrop.getElementsByClassName("hello-dialog-close")[0].addEventListener("click", cancle);
         backdrop.getElementsByClassName("hello-dialog-confirm")[0].addEventListener("click", cancle);
         backdrop.getElementsByClassName("hello-dialog-full-screen")[0].addEventListener("click", getFullScreen);
+        backdrop.getElementsByClassName("hello-dialog-header")[0].onmousedown = this.dialogMove
     }
     fullScreen = (helloDialogDom, helloDialogHeaderDom, isFullScreen) => {
         console.log("isFullScreen:", isFullScreen)
@@ -82,6 +102,62 @@ class helloDialog {
             helloDialogHeaderDom.style.cursor = "move";
         }
         this.isFullScreen = !isFullScreen;
+    };
+
+    dialogMove = (e) => {
+        console.log("eeeeeeeee:", e)
+        if (this.firstDistanceX == 0 && this.firstDistanceY == 0) {
+            this.firstDistanceX = e.clientX;
+            this.firstDistanceY = e.clientY;
+            this.distanceX = e.clientX;
+            this.distanceY = e.clientY;
+        } else {
+            this.distanceX = this.firstDistanceX;
+            this.distanceY = this.firstDistanceY;
+        }
+
+        document.onmousemove = (e) => {
+            let x = e.clientX - this.distanceX;
+            let y = e.clientY - this.distanceY;
+            let maxDomLeft = this.helloDialogDom.getBoundingClientRect().left;
+            let maxDomTop = this.helloDialogDom.getBoundingClientRect().top;
+            let maxDomRight =
+                window.innerWidth - this.helloDialogDom.getBoundingClientRect().right;
+            let maxDomBottom =
+                window.innerHeight - this.helloDialogDom.getBoundingClientRect().bottom;
+            let maxRight = x + maxDomRight;
+            let maxLeft = x - maxDomLeft;
+            let maxTop = y - maxDomTop;
+            let maxBottom = y + maxDomBottom;
+            if (x < this.tempRight) {
+                maxRight = this.tempRight;
+            }
+            if (x > this.tempLeft) {
+                maxLeft = this.tempLeft;
+            }
+            if (y > this.tempTop) {
+                maxTop = this.tempTop;
+            }
+            if (y < this.tempBottom) {
+                maxBottom = this.tempBottom;
+            }
+
+            if (x > maxLeft && x < maxRight) {
+                this.helloDialogDom.style.marginRight = `${-x * 2}px`;
+                this.tempRight = x;
+                this.tempLeft = x;
+            }
+
+            if (y > maxTop && y < maxBottom) {
+                this.helloDialogDom.style.marginBottom = `${-y * 2}px`;
+                this.tempTop = y;
+                this.tempBottom = y;
+            }
+        };
+        document.onmouseup = (e) => {
+            document.onmousemove = null;
+            document.onmouseup = null;
+        };
     };
 }
 
