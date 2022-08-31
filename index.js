1de27376e98186dd31982ccefe80216cf2b49acb
yuanbaoyaoyao <583515921@qq.com>
2022-08-31 17:15:35 +0800

    add function stretch

--- index.js
+++ index.js
@@ -9,11 +9,20 @@ class helloDialog {
         const dialogHtml = `
         <div class="hello-dialog-backdrop">
         <div class="hello-dialog">
+          <div class="right-stretch"></div>
+          <div class="top-stretch"></div>
+          <div class="bottom-stretch"></div>
+          <div class="left-stretch"></div>
+          <div class="top-right-stretch"></div>
+          <div class="top-left-stretch"></div>
+          <div class="bottom-right-stretch"></div>
+          <div class="bottom-left-stretch"></div>
           <div class="hello-dialog-header">
             <p>这是标题</p>
             <div>
               <button
                 class="hello-dialog-full-screen"
+                @click="fullScreen()"
               ></button>
               <button class="hello-dialog-close"></button>
             </div>
@@ -32,12 +41,31 @@ class helloDialog {
         const helloDialogDom = backdrop.querySelector(".hello-dialog");
         const helloDialogHeaderDom = backdrop.querySelector(".hello-dialog-header")
 
+        const rightStretchDom = backdrop.querySelector(".right-stretch");
+        const topStretchDom = backdrop.querySelector(".top-stretch");
+        const bottomStretchDom = backdrop.querySelector(".bottom-stretch");
+        const leftStretchDom = backdrop.querySelector(".left-stretch");
+        const topRightStretchDom = backdrop.querySelector(".top-right-stretch");
+        const topLeftStretchDom = backdrop.querySelector(".top-left-stretch");
+        const bottomRightStretchDom = backdrop.querySelector(".bottom-right-stretch");
+        const bottomLeftStretchDom = backdrop.querySelector(".bottom-left-stretch");
+
         let isFullScreen = false
 
         this.backdrop = backdrop
         this.helloDialogDom = helloDialogDom
         this.helloDialogHeaderDom = helloDialogHeaderDom
         this.isFullScreen = isFullScreen
+
+        this.rightStretchDom = rightStretchDom;
+        this.topStretchDom = topStretchDom;
+        this.bottomStretchDom = bottomStretchDom;
+        this.leftStretchDom = leftStretchDom;
+        this.topRightStretchDom = topRightStretchDom;
+        this.topLeftStretchDom = topLeftStretchDom;
+        this.bottomRightStretchDom = bottomRightStretchDom;
+        this.bottomLeftStretchDom = bottomLeftStretchDom;
+
 
         let distanceX = 0;
         let distanceY = 0;
@@ -48,6 +76,9 @@ class helloDialog {
         let tempTop = 0;
         let tempBottom = 0;
 
+        let tempDomWidth = 0;
+        let tempDomHeight = 0;
+
         this.distanceX = distanceX;
         this.distanceY = distanceY;
         this.firstDistanceX = firstDistanceX;
@@ -56,6 +87,9 @@ class helloDialog {
         this.tempLeft = tempLeft;
         this.tempTop = tempTop;
         this.tempBottom = tempBottom;
+
+        this.tempDomHeight = tempDomHeight;
+        this.tempDomWidth = tempDomWidth;
 
     }
     show = (backdrop) => {
@@ -81,7 +115,15 @@ class helloDialog {
         backdrop.getElementsByClassName("hello-dialog-close")[0].addEventListener("click", cancle);
         backdrop.getElementsByClassName("hello-dialog-confirm")[0].addEventListener("click", cancle);
         backdrop.getElementsByClassName("hello-dialog-full-screen")[0].addEventListener("click", getFullScreen);
-        backdrop.getElementsByClassName("hello-dialog-header")[0].onmousedown = this.dialogMove
+        this.helloDialogHeaderDom.onmousedown = this.dialogMove
+        this.rightStretchDom.onmousedown = this.dialogStretch;
+        this.topStretchDom.onmousedown = this.dialogStretch;
+        this.bottomStretchDom.onmousedown = this.dialogStretch;
+        this.leftStretchDom.onmousedown = this.dialogStretch;
+        this.topRightStretchDom.onmousedown = this.dialogStretch;
+        this.topLeftStretchDom.onmousedown = this.dialogStretch;
+        this.bottomRightStretchDom.onmousedown = this.dialogStretch;
+        this.bottomLeftStretchDom.onmousedown = this.dialogStretch;
     }
     fullScreen = (helloDialogDom, helloDialogHeaderDom, isFullScreen) => {
         console.log("isFullScreen:", isFullScreen)
@@ -152,6 +194,82 @@ class helloDialog {
                 this.helloDialogDom.style.marginBottom = `${-y * 2}px`;
                 this.tempTop = y;
                 this.tempBottom = y;
+            }
+        };
+        document.onmouseup = (e) => {
+            document.onmousemove = null;
+            document.onmouseup = null;
+        };
+    };
+    dialogStretch = (e) => {
+        let tempStretchX = e.clientX;
+        let tempStretchY = e.clientY;
+        let tempWidth = this.helloDialogDom.getBoundingClientRect().width;
+        let tempHeight = this.helloDialogDom.getBoundingClientRect().height;
+
+        let tempClassName = e.srcElement.className;
+
+        document.onmousemove = (e) => {
+            let domWidth = this.helloDialogDom.getBoundingClientRect().width;
+            let domHeight = this.helloDialogDom.getBoundingClientRect().height;
+            if (e.clientX < domWidth && e.clientX > 0) {
+                domWidth = e.clientX;
+            }
+            if (e.clientY < domHeight && e.clientY > 0) {
+                domHeight = e.clientY;
+            }
+
+            if (tempClassName == "right-stretch" && domWidth < window.innerWidth) {
+                this.helloDialogDom.style.width =
+                    `${tempWidth + (e.clientX - tempStretchX) * 2}` + "px";
+                this.tempDomWidth = domWidth;
+            }
+            if (tempClassName == "top-stretch" && domHeight < window.innerHeight) {
+                this.helloDialogDom.style.height =
+                    `${tempHeight - (e.clientY - tempStretchY) * 2}` + "px";
+                this.tempDomHeight = domHeight;
+            }
+            if (tempClassName == "bottom-stretch" && domHeight < window.innerHeight) {
+                this.helloDialogDom.style.height =
+                    `${tempHeight + (e.clientY - tempStretchY) * 2}` + "px";
+                this.tempDomHeight = domHeight;
+            }
+            if (tempClassName == "left-stretch" && domWidth < window.innerWidth) {
+                this.helloDialogDom.style.width =
+                    `${tempWidth - (e.clientX - tempStretchX) * 2}` + "px";
+                this.tempDomWidth = domWidth;
+            }
+            if (tempClassName == "top-left-stretch") {
+                this.helloDialogDom.style.width =
+                    `${tempWidth - (e.clientX - tempStretchX) * 2}` + "px";
+                this.helloDialogDom.style.height =
+                    `${tempHeight - (e.clientY - tempStretchY) * 2}` + "px";
+                this.tempDomHeight = domHeight;
+                this.tempDomWidth = domWidth;
+            }
+            if (tempClassName == "top-right-stretch") {
+                this.helloDialogDom.style.width =
+                    `${tempWidth + (e.clientX - tempStretchX) * 2}` + "px";
+                this.helloDialogDom.style.height =
+                    `${tempHeight - (e.clientY - tempStretchY) * 2}` + "px";
+                this.tempDomHeight = domHeight;
+                this.tempDomWidth = domWidth;
+            }
+            if (tempClassName == "bottom-left-stretch") {
+                this.helloDialogDom.style.width =
+                    `${tempWidth - (e.clientX - tempStretchX) * 2}` + "px";
+                this.helloDialogDom.style.height =
+                    `${tempHeight + (e.clientY - tempStretchY) * 2}` + "px";
+                this.tempDomHeight = domHeight;
+                this.tempDomWidth = domWidth;
+            }
+            if (tempClassName == "bottom-right-stretch") {
+                this.helloDialogDom.style.width =
+                    `${tempWidth + (e.clientX - tempStretchX) * 2}` + "px";
+                this.helloDialogDom.style.height =
+                    `${tempHeight + (e.clientY - tempStretchY) * 2}` + "px";
+                this.tempDomHeight = domHeight;
+                this.tempDomWidth = domWidth;
             }
         };
         document.onmouseup = (e) => {
