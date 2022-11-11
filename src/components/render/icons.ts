450f856b60aa61192c08f42d293dec54372b05fe
yuanbaoyaoyao <583515921@qq.com>
2022-11-11 14:43:33 +0800

    add error icon

--- src/components/render/icons.ts
+++ src/components/render/icons.ts
@@ -7,17 +7,23 @@ const { ICON_CONTAINER, ICON_ERROR, ICON_INFO, ICON_QUESTION, ICON_SUCCESS, ICON
 const renderIcons = (options: IconOptions): void => {
     const iconContainer: HTMLElement = getNode(ICON_CONTAINER)
     let icon: HTMLElement = document.createElement("div")
+    let iconHtml: string
     switch (options.iconName) {
         case 'success':
             iconContainer.classList.add(ICON_SUCCESS)
-            const iconHtml = `
+            iconHtml = `
             <div class="${ICON_SUCCESS}-short-line"></div>
             <div class="${ICON_SUCCESS}-long-line"></div>
             `
             icon.innerHTML = iconHtml
             break;
         case 'error':
-            icon.className = ICON_ERROR
+            iconContainer.classList.add(ICON_ERROR)
+            iconHtml = `
+            <div class="${ICON_ERROR}-line-one"></div>
+            <div class="${ICON_ERROR}-line-two"></div>
+            `
+            icon.innerHTML = iconHtml
             break;
         case 'info':
             icon.className = ICON_INFO
